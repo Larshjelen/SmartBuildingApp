750dc9104fdabc3f59726a114d3c637428bac729
amkh <ammarkhalil08@gmail.com>
2022-03-21 12:57:23 +0100

    Initial Commit

---
+++ SmartBuildingUITests/SmartBuildingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SmartBuildingUITestsLaunchTests.swift
+//  SmartBuildingUITests
+//
+//  Created by Ammar Khalil on 21/03/2022.
+//
+
+import XCTest
+
+class SmartBuildingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
