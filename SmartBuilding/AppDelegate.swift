b18948e1003525e8eab43973dbef98a7449e148f
amkh <ammarkhalil08@gmail.com>
2022-04-11 10:09:20 +0200

    added mapviewconqroller

--- SmartBuilding/AppDelegate.swift
+++ SmartBuilding/AppDelegate.swift
@@ -6,10 +6,42 @@
 //
 
 import UIKit
-import CoreData
+import AppAuth
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+    var window: UIWindow?
+    
+    var currentAuthorizationFlow: OIDExternalUserAgentSession?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // CHANGE:
+        // Add your Google Maps API key here:
+        GMSServices.provideAPIKey("AIzaSyArBUnnKq2JNGSuTUuFwWjaYiUgRJldYqs")
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      if currentAuthorizationFlow?.resumeExternalUserAgentFlow(with: url) ?? false {
+        currentAuthorizationFlow = nil
+        return true
+      }
+      return false
+    }
 
 
 
--- SmartBuilding/Controllers/MapViewController.swift
+++ SmartBuilding/Controllers/MapViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 import PhoneFUEL
+import CoreLocation
+import CoreBluetooth
+import PhoneFUELGoogleMaps
 
 class MapViewController: UIViewController, SPFLocationManagerDelegate {
 
@@ -25,11 +28,48 @@ class MapViewController: UIViewController, SPFLocationManagerDelegate {
             return
             
         }
-        
-        
-
-        
 
         manager!.startUpdatingLocation();
+        print(manager?.getState()!)
+        nextStartupStep()
+    }
+    
+    private func nextStartupStep() {
+        
+        DispatchQueue.main.async {
+            // Check location settings
+            if self.needInput() {
+                self.requestInput()
+            } else if self.locationAuthorizationStatus == nil {
+                // Wait for locationAuthorizationStatus to be set
+                self.nextStartupStep()
+            } else if self.locationAuthorizationStatus == .denied {
+                let alertController = UIAlertController (title: "Location denied", message: "Go to Settings?", preferredStyle: .alert)
+                let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+                    guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                        return
+                    }
+                    if UIApplication.shared.canOpenURL(settingsUrl) {
+                        UIApplication.shared.open(settingsUrl, completionHandler: nil)
+                    }
+                }
+                alertController.addAction(settingsAction)
+                let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+                alertController.addAction(cancelAction)
+                self.present(alertController, animated: true, completion: nil)
+            } else if self.locationAuthorizationStatus == .restricted {
+                let alertController = UIAlertController (title: "Location restricted", message: "Need access to location", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+                alertController.addAction(cancelAction)
+                self.present(alertController, animated: true, completion: nil)
+            } else if self.locationAuthorizationStatus == .notDetermined {
+                self.requestingLocationAuth = true
+                self.clLocationManager.requestAlwaysAuthorization()
+            } else if self.needAuthentication() {
+                self.requestAuthentication()
+            } else {
+                self.startPhoneFUEL()
+            }
+        }
     }
 }
