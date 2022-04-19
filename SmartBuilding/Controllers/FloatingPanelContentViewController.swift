680b2d19d9dae0d80a251f125053221aeff3f0bf
amkh <ammarkhalil08@gmail.com>
2022-04-19 15:41:49 +0200

    custom url

--- SmartBuilding/Controllers/FloatingPanelContentViewController.swift
+++ SmartBuilding/Controllers/FloatingPanelContentViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import SafariServices
 
-class FloatingPanelContentViewController: UIViewController {
+class FloatingPanelContentViewController: UIViewController, SFSafariViewControllerDelegate {
+
+    let authUrl =  "https://api.entraos.io/person​/persons/authorize?clientId=g3PLRTK4OAnTWQGd0vbaGG.Cp7GlNLFyE9hA26qP5r0-&applicationName=Rebel Wayfinder&applicationUrl=http://localhost/&redirectUrl=SmartBuildingAuth://"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    @IBAction func authBtn(_ sender: UIButton) {
+        
+        print("safari")
+        if let url = URL(string: authUrl){
+            UIApplication.shared.open(url)
+        }
+        
     }
     
 }
