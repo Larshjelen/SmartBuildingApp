e930ba57e0cfce6cea89ea10c2fedbd2d42d78a7
amkh <ammarkhalil08@gmail.com>
2022-05-16 17:07:07 +0200

    send data to booking summary

--- SmartBuilding/Controllers/MeetingRooms/BookingSummaryViewController.swift
+++ SmartBuilding/Controllers/MeetingRooms/BookingSummaryViewController.swift
@@ -9,19 +9,43 @@ import UIKit
 
 class BookingSummaryViewController: UIViewController {
     
+    
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var date: UILabel!
+    
+    @IBOutlet weak var time: UILabel!
+    
+    @IBOutlet weak var capacity: UILabel!
+    
+    
+    @IBOutlet weak var price: UILabel!
+    
     var meetingRomName : String?
     var meetingRoomCapacity : String?
     var meetingRoomPrice : String?
-    var bookingDate : String
-    var bookingTimeFrom : String
-    var bookingTimeTil : String
+    var bookingDate : String?
+    var bookingTimeFrom : String?
+    var bookingTimeTil : String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         guard let selectedMeetingRoomName = meetingRomName else {return}
+        guard let selectedMeetingRoomCapacity = meetingRoomCapacity else {return}
+        guard let selectedMeetingRoomPrice = meetingRoomPrice else {return}
+        guard let selectedMeetingRoomDate = bookingDate else {return}
+        guard let selectedMeetingRoomTimeFrom = bookingTimeFrom else {return}
+        guard let selectedMeetingRoomTimeTil = bookingTimeTil else {return}
         
-        print(selectedMeetingRoomName)
+        name.text = selectedMeetingRoomName
+        date.text = selectedMeetingRoomDate
+        time.text = "\(selectedMeetingRoomTimeFrom):\(selectedMeetingRoomTimeTil)"
+        price.text = selectedMeetingRoomPrice
+        capacity.text = selectedMeetingRoomCapacity
+        
+       
     }
     
 
