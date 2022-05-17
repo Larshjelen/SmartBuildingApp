d6d8e5a9cce83ef4a2864f9a8ab05e4bba8293de
amkh <ammarkhalil08@gmail.com>
2022-05-17 19:01:54 +0200

    Search meeting room data

--- SmartBuilding/Models/MeetingRoomSearch.swift
+++ SmartBuilding/Models/MeetingRoomSearch.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct MeetingRoomSearch : Codable {
+    
+    let rooms : [Room]
+}
+
+struct Room : Codable {
+    
+    let name : String
+    let location : Location
+}
+
+struct Location : Codable {
+    
+    let lat : Double
+    let long : Double
+}
